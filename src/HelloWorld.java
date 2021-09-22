1f8c5022e5c94bffcc3df9f830fc34f50e9b8375
raihanromzi <raihan.rakhman16@gmail.com>
2021-09-23 04:39:27 +0700

    Hello world, int variable, operator

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello, Raihan");
+
+        int myFirstNumber = (5 + 5) + (5 * 5);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.print("First Number = ");
+        System.out.println(myFirstNumber);
+
+        System.out.print("Second Number = ");
+        System.out.println(mySecondNumber);
+
+        System.out.print("Third Number = ");
+        System.out.println(myThirdNumber);
+
+        System.out.print("Total = ");
+        System.out.println(myTotal);
+    }
+
+}

8a39c1e3fcb22fe49cb05495d455b9301f3da98c
raihanromzi <raihan.rakhman16@gmail.com>
2021-09-25 08:49:06 +0700

    primitive variable

--- src/Variables.java
+++ src/Variables.java
@@ -1,37 +1,38 @@
+import java.sql.SQLOutput;
+
 public class Variables {
     public static void main(String[] args){
 
-//      BYTE VARIABLE (8 BITS)
-        byte myMinByteValue = Byte.MIN_VALUE;
-        byte myMaxByteValue = Byte.MAX_VALUE;
-
+//      BYTE (8 BITS)
+        byte myMinByteValue = Byte.MIN_VALUE; //-128
+        byte myMaxByteValue = Byte.MAX_VALUE; //127
         System.out.println("Byte Maximum Value = " + myMaxByteValue);
         System.out.println("Byte Minimum Value = " + myMinByteValue);
 
 
-//      SHORT VARIABLE (16 BITS)
-        short myMinShortValue = Short.MIN_VALUE;
-        short myMaxShortValue = Short.MAX_VALUE;
-
+//      SHORT (16 BITS)
+        short myMinShortValue = Short.MIN_VALUE; //-32768
+        short myMaxShortValue = Short.MAX_VALUE; //32767
         System.out.println("Short Maximum Value = " + myMaxShortValue);
         System.out.println("Short Minimum Value = " + myMinShortValue);
 
 
-//      INTEGER VARIABLE (32 BITS)
+//      INTEGER (32 BITS)
         int myNum = 10000;
-        int myMinIntValue = Integer.MIN_VALUE;
-        int myMaxIntValue = Integer.MAX_VALUE;
+        int myMinIntValue = Integer.MIN_VALUE; //-2147483648
+        int myMaxIntValue = Integer.MAX_VALUE; //2147483647
 
 //      USING "_" TO MAKE IT EASIER TO READ
         int myMaxIntValueTest = 2_147_483_647;
-
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+//      OVERFLOW AND UNDERFLOW
         System.out.println("Max int Value + 1 = " + (myMaxIntValue + 1));
         System.out.println("Min int Value + 1 = " + (myMinIntValue - 1));
 
 
-//      LONG VARIABLE (64 BITS)
+//      LONG (64 BITS)
         long myLongNumber = 100L;
 
 //      Without L in value = 100, Java detect as INT
@@ -40,9 +41,8 @@ public class Variables {
 //      long bigNumInt = 2_147_438_647_123; ERROR
 
         long bigNumInt = 2_147_438_647_123L;
-        long myMaxLongValue = Long.MAX_VALUE;
-        long myMinLongValue = Long.MIN_VALUE;
-
+        long myMaxLongValue = Long.MAX_VALUE; //9223372036854775807
+        long myMinLongValue = Long.MIN_VALUE; //-9223372036854775808
         System.out.println("Long Maximum Value = " + myMaxLongValue);
         System.out.println("Long Minimum Value = " + myMinLongValue);
 
@@ -50,7 +50,8 @@ public class Variables {
         System.out.println("Long Maximum + 1 = " + (myMaxLongValue + 1));
         System.out.println("Long Minimum + 1 = " + (myMinLongValue - 1));
 
-//      Casting
+
+//      CASTING
         int myTotal = (myMinIntValue / 2);
         System.out.println(myTotal);
 
@@ -60,5 +61,68 @@ public class Variables {
 
         short myNewShort = (short) (myMinShortValue / 2);
         System.out.println("Casting INT to Short = " + myNewShort);
+
+
+//      FLOAT (32) AND DOUBLE (64)
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinFloatValue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Value = " + myMinDoubleValue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
+
+
+//      Error because java always treated as double variable
+//      float javaAlwaysUseDouble = 5.34;
+//      Casting to tell java to convert to float
+        float myFloatValue = (float) 5.25;
+        float myFloatValue2 = 5.25f;
+        int myIntValue = 5 / 3;
+
+//      Double is preferred
+        double myDoubleValue = 5.25d;
+        float floatValue = 5f / 3f; //1.6666666
+        double doubleValue = 5d / 3d; //1.666666666666666667 //5.00 and 3.00 java will assume as Double auto
+        System.out.println("My Int Value = " + myIntValue);
+        System.out.println("My Float Value = " + floatValue);
+        System.out.println("My Double Value = " + doubleValue);
+
+
+//      CHAR (16) AND BOOLEAN
+        char myChar = 'C';
+        char myUnicode = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+
+        boolean myBooleanT = true;
+        boolean myBooleanF = false;
+
+
+//      STRING
+        String myString = "This String";
+        int myNum2 = 50;
+        String myNumString = "10";
+        System.out.println("My string is " + myString);
+        System.out.println(myNum2 + myNumString);
+
+
+
+//      PRIMITIVE TYPE CHALLENGE
+        byte byteNum = 10;
+        short shortNum = 20;
+        int intNum = 50;
+        long totalNumLong = (50_000L + (10L * (byteNum + shortNum + intNum)));
+        short totalNumShort = (short) (1000 + 10 * (byteNum + shortNum + intNum));
+
+        System.out.println(totalNumLong);
+        System.out.println(totalNumShort);
+
+//      CHALLENGE
+        double poundValue = 2d;
+        double kilogramValue = 0.45359237d;
+        System.out.println(poundValue * kilogramValue);
     }
 }
